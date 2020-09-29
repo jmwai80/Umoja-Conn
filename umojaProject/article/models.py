c4876fb25641fb99699b8cc11b817489db9b5063
John Mwai <jmwai@conncol.edu>
2020-09-29 14:30:52 -0400

    reverted models

--- umojaProject/article/models.py
+++ umojaProject/article/models.py
@@ -1,16 +1,16 @@
 from django.db import models
 from ckeditor.fields import RichTextField
+from trix.fields import TrixField
+
 # Create your models here.
 
 class Article(models.Model):
-    author =models.CharField(max_length = 500,verbose_name = "Writer")
-    title = models.CharField(max_length = 500,verbose_name = "Title")
+    author = models.ForeignKey("auth.User",on_delete = models.CASCADE,verbose_name = "Writer ")
+    title = models.CharField(max_length = 50,verbose_name = "Title")
     content = RichTextField()
     created_date = models.DateTimeField(auto_now_add=True,verbose_name="Creation Date")
-    article_image = models.FileField(upload_to='img', blank = True,null = True,verbose_name="Add Photos to Article")
-    published = models.BooleanField(default=False)
-    slug = models.SlugField(unique=True, max_length=100,blank=True)
-
+    article_image = models.FileField(blank = True,null = True,verbose_name="Add Photos to Article")
+    slug = models.SlugField(unique=True, max_length=100)
 
     def __str__(self):
         return self.title
@@ -31,40 +31,3 @@ class Comment(models.Model):
         return self.comment_content
     class Meta:
         ordering = ['-comment_date']
-
-class Program(models.Model):
-    program_company = models.CharField(max_length=100, null=True)
-    diversity_program = models.CharField(max_length=100, null=True)
-    diversity_link = models.CharField(max_length=100, null=True)
-    diversity_industry = models.CharField(max_length=100, null=True)
-    diversity_population = models.CharField(max_length=100, null=True)
-    date_posted = models.DateTimeField(auto_now_add=True,verbose_name="Posted Date")
-    admin_approved = models.BooleanField(default=False)
-
-class Internship(models.Model):
-    internship_company = models.CharField(max_length=100, null=True)
-    name = models.CharField(max_length=100, null=True)
-    link = models.CharField(max_length=100, null=True)
-    industry = models.CharField(max_length=100, null=True)
-    location = models.CharField(max_length=100, null=True)
-    date_posted = models.DateTimeField(auto_now_add=True,verbose_name="Date")
-    admin_approved = models.BooleanField(default=False)
-
-class Fellowship(models.Model):
-    name = models.CharField(max_length=100, null=True)
-    link =  models.CharField(max_length=100, null=True)
-    location = models.CharField(max_length=100, null=True)
-    date_posted = models.DateTimeField(auto_now_add=True,verbose_name="Posted")
-    admin_approved = models.BooleanField(default=False)
-
-class Resource(models.Model):
-    title = models.CharField(max_length=100, null=True)
-    description = models.CharField(max_length=1000, null=True)
-    content = RichTextField()
-    admin_approved = models.BooleanField(default=False)
-
-class Gallery(models.Model):
-    image = models.ImageField(upload_to='gallery', null=True, blank=True)
-    eventName = models.CharField(max_length=1000, null=True,blank=True)
-    eventDate = models.CharField(max_length=1000, null=True, blank=True)
-    admin_approved = models.BooleanField(default=False)
